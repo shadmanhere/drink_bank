ae747fea82cbd721ae2592e2214e1bac76a2a540
Shadman <shadman005@gmail.com>
2019-02-10 21:00:43 +0530

    2nd commit 2

---
+++ build/precache-manifest.c58bceb3d0a57391328e3a19df00a9d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c96f89131c86b051c6f7",
+    "url": "/static/js/main.c96f8913.chunk.js"
+  },
+  {
+    "revision": "53bb256d01b3ab93d59a",
+    "url": "/static/js/1.53bb256d.chunk.js"
+  },
+  {
+    "revision": "c96f89131c86b051c6f7",
+    "url": "/static/css/main.ae0c3649.chunk.css"
+  },
+  {
+    "revision": "53bb256d01b3ab93d59a",
+    "url": "/static/css/1.221085aa.chunk.css"
+  },
+  {
+    "revision": "316ab5886a76533d25046c049e1aaae9",
+    "url": "/index.html"
+  }
+];
